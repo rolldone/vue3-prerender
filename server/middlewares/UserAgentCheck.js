323ca7a4071669025e22630efa590233b9ca970d
donny rolanda <donny.rolanda@gmail.com>
2020-11-11 08:04:20 +0800

    #ondevelop #prerender - Introduce official v1 prerender technique

--- server/middlewares/UserAgentCheck.js
+++ server/middlewares/UserAgentCheck.js
@@ -26,67 +26,67 @@ var runnerBrowser = (async function(){
   browser.on('disconnected', runnerBrowser);
 });
 runnerBrowser();
+
 module.exports = async function(req,res,next){
   console.log('req',req.headers.referer);
   const local_url = req.headers.referer || req.protocol + "://" + req.get('host') + req.path;
   console.log('local_url',local_url);  
-
   console.log('user-agent -> ',req.headers['user-agent']);
   let checkExist = req.headers['user-agent'].match(/MY_SYSTEM/g) || [];
-  // console.log('checkExist -> ',checkExist);
-  // if(!isBot(req.headers['user-agent'])){
-  //   let Lighthouse = req.headers['user-agent'].match(/Chrome-Lighthouse/g) || [];
-  //   if(Lighthouse.length == 0){
-  //     // console.log('Bukan Crawling');
-  //     return next();
-  //   }
-  // }
+
+  /* Only real user agent get block at here */
+  if(!isBot(req.headers['user-agent'])){
+    let Lighthouse = req.headers['user-agent'].match(/Chrome-Lighthouse/g) || [];
+    if(Lighthouse.length == 0){
+      return next();
+    }
+  }
+  
   var page = null;
   if (checkExist.length > 0) {
-    // console.log('Deteck Dari Crawling');
     next();
   } else {
-      try {
-          let html = null;
-          if(Cache[local_url] != null){
-              console.log('Cache -> ',local_url);
-              res.send(Cache[local_url]);
-              return;
-          }
-          page = await browser.newPage();
-          await page.setDefaultNavigationTimeout(60000);
-          await page.setUserAgent(req.headers['user-agent']+' MY_SYSTEM');
-          switch (config.env) {
-            case "development":
-            case "dev":
-              /* Karena pake hammer SPA jangan gunakan waitUntil */
-              await page.goto(local_url);
-              break;
-            case "production":
-            case "devserver":
-              await page.goto(local_url,{
-                waitUntil: "networkidle0"
-              });
-              break;
-          }
-            
-          // await page.waitForSelector('#mapsingleid');    
-          html = await page.evaluate(() => {
-              return document.documentElement.innerHTML;
-          });
-          Cache[local_url] = html;
-          // await page.goto('about:blank');
-          await page.close();
-          closeOne();
-          res.send(html);
-          return;
-      } catch (err) {
-          // await page.goto('about:blank');
-          await page.close();
-          closeOne();
-          console.log('content -> ',err);
-          next();
-      }
+    try {
+        let html = null;
+        if(Cache[local_url] != null){
+            console.log('Cache -> ',local_url);
+            res.send(Cache[local_url]);
+            return;
+        }
+        page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(60000);
+        await page.setUserAgent(req.headers['user-agent']+' MY_SYSTEM');
+        switch (config.env) {
+          case "development":
+          case "dev":
+            /* Karena pake hammer SPA jangan gunakan waitUntil */
+            await page.goto(local_url);
+            break;
+          case "production":
+          case "devserver":
+            await page.goto(local_url,{
+              waitUntil: "networkidle0"
+            });
+            break;
+        }
+          
+        // await page.waitForSelector('#mapsingleid');    
+        html = await page.evaluate(() => {
+            return document.documentElement.innerHTML;
+        });
+        Cache[local_url] = html;
+        // await page.goto('about:blank');
+        await page.close();
+        closeOne();
+        res.send(html);
+        return;
+    } catch (err) {
+        // await page.goto('about:blank');
+        await page.close();
+        closeOne();
+        console.log('content -> ',err);
+        next();
+    }
   }
 }
 
