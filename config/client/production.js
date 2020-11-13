3f4f571875ddbef6ac8120a1134ee171f4b8c267
donny rolanda <donny.rolanda@gmail.com>
2020-11-13 20:05:06 +0800

    #ondevelop - update config production

--- config/client/production.js
+++ config/client/production.js
@@ -1,14 +1,14 @@
 let env = {
-	APP_NAME : 'ARTYPLANET_LOCAL',
+	APP_NAME : 'ARTYPLANET_PRODUCTION',
     DIST_URL: '/public',
-    API_URL: 'http://service.artyplanet.local/',
-    API_ASSET_URL : 'http://service.artyplanet.local',
+    API_URL: 'https://service.artyplanet.io',
+    API_ASSET_URL : 'https://service.artyplanet.io',
     HTTP_GENERATOR_URL : 'http://localhost:1084',
     BASE_PATH: '/',
     /* Ini mengikuti new Copy Plugin */
     ASSET : '/public',
     g_captcha_site_key : '6Lef5tUUAAAAANDvzGnYRQ-YdO8XjLPRrDyEnNsd',
-    API_LIST : 'http://service.artyplanet.local/v1/member/routes',
+    API_LIST : 'https://service.artyplanet.io/v1/member/routes',
     MAPBOX_ACCESS_TOKEN : 'pk.eyJ1IjoiYWthc2FkZXYiLCJhIjoiY2toNDNycTkxMDI2dTJzbWxubGxyOHlndiJ9.e9-jCWxYxk5m5A2nLymbTw'
 };
 module.exports = env;
