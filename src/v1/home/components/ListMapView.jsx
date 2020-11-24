1854136176a9569a0f6983ae398c913bad2ab116
donny rolanda <donny.rolanda@gmail.com>
2020-11-24 18:57:31 +0800

    #ondevelop develop product detail develop business

--- src/v1/home/components/ListMapView.jsx
+++ src/v1/home/components/ListMapView.jsx
@@ -1,13 +1,24 @@
 import BaseVue from "../../../base/BaseVue";
 import { reactive } from 'vue';
 import config from '@config';
+import BusinessService from "../../services/BusinessService";
+import ProductService from "../../services/ProductService";
 
 export const ListMMapViewClass = BaseVue.extend({
   data : function(){
     return reactive({
       action : 'list',
-      select_business : null
+      select_business_id : null,
+      business_data : null,
+      product_datas : [],
+      query : {}
     });
+  },
+  returnBusinessService : function(){
+    return BusinessService.create();
+  },
+  returnProductService : function(){
+    return ProductService.create();
   },
   construct : function(props,context){
     let self = this;
@@ -16,29 +27,67 @@ export const ListMMapViewClass = BaseVue.extend({
   setInitDOMSelection : function(action,props){
 
   },
-  selectBusiness : async function(id){
+  // selectBusiness : async function(id){
+  //   let self = this;
+  //   window.staticType(id,[Number]);
+  //   let { marker_datas } = self.props;
+  //   let select_business = (function(parseDatas){
+  //     let select_business = null;
+  //     for(var a=0;a<parseDatas.length;a++){
+  //       if(id == parseDatas[a].id){
+  //         select_business = parseDatas[a];
+  //         break;
+  //       }
+  //     }
+  //     return select_business;
+  //   })(marker_datas);
+  //   if(select_business == null) return;
+  //   await self.setUpdate('select_business',select_business);
+  //   await self.setUpdate('action','detail');
+  // },
+  getProducts : async function(){
     let self = this;
-    window.staticType(id,[Number]);
-    let { marker_datas } = self.props;
-    let select_business = (function(parseDatas){
-      let select_business = null;
-      for(var a=0;a<parseDatas.length;a++){
-        if(id == parseDatas[a].id){
-          select_business = parseDatas[a];
-          break;
-        }
-      }
-      return select_business;
-    })(marker_datas);
-    if(select_business == null) return;
-    await self.setUpdate('select_business',select_business);
-    await self.setUpdate('action','detail');
+    try{
+      let { query } = this.get();
+      let service = this.returnProductService();
+      let resData = await service.getProducts(query);
+      return resData;
+    }catch(ex){
+      console.error('getProducts - ex ',ex);
+    }
+  },
+  setProducts : function(props){
+    let self = this;
+    if(props == null) return;
+    let products = (function(parseData){
+      return parseData;
+    })(props.return);
+    self.setUpdate('product_datas',products);
+  },
+  getBusiness : async function(){
+    let self = this;
+    try{
+      let select_business_id = self.get('select_business_id');
+      let service = this.returnBusinessService();
+      let resData = await service.getBusiness(select_business_id);
+      return resData;
+    }catch(ex){
+      console.error('getBusiness - ex ',ex);
+    }
+  },
+  setBusiness : function(props){
+    let self = this;
+    if(props == null) return;
+    let business_data = (function(parseData){
+      return parseData;
+    })(props.return);
+    self.setUpdate('business_data',business_data);
   },
   setOnChangeListener : function(func){
     let self = this;
     self.onChangeListener = func;
   },
-  handleClick : function(action,props,e){
+  handleClick : async function(action,props,e){
     let self = this;
     switch(action){
       case 'BACK':
@@ -47,7 +96,16 @@ export const ListMMapViewClass = BaseVue.extend({
         break;
       case 'OPEN_DETAIL':
         e.preventDefault();
-        self.selectBusiness(props.id);
+        window.staticType(props,[Object]);
+        window.staticType(props.id,[Number]);
+        window.staticType(props.search,[null,String]);
+        await self.setUpdate('query',{
+          business_id : props.id,
+          search : props.search
+        });
+        await self.setUpdate('select_business_id',props.id);
+        self.setBusiness(await self.getBusiness());
+        self.setProducts(await self.getProducts());
         self.onChangeListener('BUSINESS_SELECTED',self.get('select_business'),props.index);
         break;
     }
@@ -79,7 +137,7 @@ export default {
               <div class="asl_1">
                 <div class="asl_a_11 on_mobile">
                   <div class="asl_11 on_mobile">
-                    <img style={{ "background-image" : "url("+config.ARTYWIZ_HOST+markerItem.products[0].photo+")"}} alt=""/>
+                    <img style={{ "background-image" : "url("+config.ARTYWIZ_HOST+markerItem.store.icon+")"}} alt=""/>
                   </div>
                   <div class="asl_12">
                     <div class="asl_121 on_mobile">
@@ -95,39 +153,46 @@ export default {
                     </div>
                   </div>
                 </div>
-                {markerItem.products.length > 0?
-                  <div class="asl_13">
-                    {(()=>{
-                      let subItem = [];
-                      for(var b=0; b < markerItem.products.length;b++){
-                        let productItem = markerItem.products[b];
-                        subItem.push(<div class="app_shop_detail_list">
-                          <div class="asdl_1">
-                            <img style={{ "background-image" : "url("+config.ARTYWIZ_HOST+productItem.photo+")"}} alt=""/>
-                          </div>
-                          <div class="asdl_2">
-                            <div class="asdl_21">
-                              <h4>{productItem.name}</h4>
-                              <div class="asdl_211">
-                                <span>{productItem.price}€ TTC</span>
-                                <span class="divider"></span>
-                                <span>{productItem.price_promotion}€ TTC</span>
+                {(()=>{
+                  if(markerItem.products != null){
+                    if(markerItem.products.length > 0){
+                      return (<div class="asl_13">
+                        {(()=>{
+                          let subItem = [];
+                          for(var b=0; b < markerItem.products.length;b++){
+                            let productItem = markerItem.products[b];
+                            subItem.push(<div class="app_shop_detail_list">
+                              <div class="asdl_1">
+                                <img style={{ "background-image" : "url("+config.ARTYWIZ_HOST+productItem.photo+")"}} alt=""/>
                               </div>
-                            </div>
-                            <div class="asdl_22">
-                              <span>Description</span>
-                              <span>{productItem.description}</span>
-                            </div>
-                            <div class="asdl_23">
-                              <span>Ingrédients</span>
-                              <span>{productItem.ingredient}</span>
-                            </div>
-                          </div>
-                        </div>);
-                      }
-                      return subItem;
-                    })()}
-                  </div>:null}
+                              <div class="asdl_2">
+                                <div class="asdl_21">
+                                  <h4>{productItem.name}</h4>
+                                  <div class="asdl_211">
+                                    <span>{productItem.price}€ TTC</span>
+                                    <span class="divider"></span>
+                                    <span>{productItem.price_promotion}€ TTC</span>
+                                  </div>
+                                </div>
+                                <div class="asdl_22">
+                                  <span>Description</span>
+                                  <span>{productItem.description}</span>
+                                </div>
+                                <div class="asdl_23">
+                                  <span>Ingrédients</span>
+                                  <span>{productItem.ingredient}</span>
+                                </div>
+                              </div>
+                            </div>);
+                          }
+                          return subItem;
+                        })()}
+                      </div>);
+                    }else{
+                      return null;
+                    }
+                  }
+                })()}
               </div>
             );
           }
