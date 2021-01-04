ca3189ce8a864713231c732bc400497ef3ac9ff8
rolldone <donny.rollproject@gmail.com>
2021-01-04 09:05:17 +0800

    Update StaticType.js
    - [BIG CHANGE] : Big improvement, change workflow rule code
    - Have native object support checking

--- src/base/StaticType.js
+++ src/base/StaticType.js
@@ -1,31 +1,130 @@
 (function(global){
+  var typeVariable = function(){
+    var types = {
+      'get': function(prop) {
+         return Object.prototype.toString.call(prop);
+      },
+      'null': '[object Null]',
+      'object': '[object Object]',
+      'array': '[object Array]',
+      'string': '[object String]',
+      'boolean': '[object Boolean]',
+      'number': '[object Number]',
+      'date': '[object Date]',
+      'function': '[object Function]',
+      'mouseevent': '[object MouseEvent]',
+      'keyboardevent': '[object KeyboardEvent]'
+    };
+    return {
+      check : function(props){
+        return types.get(props);
+      },
+      types
+    };
+  };
   /* Static Type check allowed type data */
-  let staticType = function (inVariable, typeDatas = []) {
+  let staticType = function (inVariable, typeDatas = [],debug=false) {
     var isWRong = true;
     var closureCondition = function (theVariable, arrayRecordTypeOf) {
       return function (typeDataItem) {
-        switch (true) {
-          case typeDataItem == Array:
-            return Array.isArray(theVariable);
-          case typeDataItem == undefined:
-          case typeDataItem == null:
-            if (theVariable == typeDataItem) {
-              return true;
+        if(typeDataItem == null){
+          typeDataItem = "Null";
+        }else{
+          typeDataItem = typeDataItem.name+'';
+        }
+        if(theVariable == undefined){
+          theVariable = null;
+        }
+        var getType = typeVariable();
+        if(debug == true){
+          debugger;
+        }
+        if(debug == true){
+          debugger;
+        }
+        let result = false;
+        switch(true){
+          case typeDataItem.toLowerCase() == 'null':
+            result = getType.check(theVariable) == getType.types.null;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
             }
-            arrayRecordTypeOf.push(typeDataItem);
-            return false;
-          case typeof theVariable == typeDataItem.name.toLowerCase():
-            return true;
+            return result;
+          case typeDataItem.toLowerCase() == 'function':
+            result = getType.check(theVariable) == getType.types.function;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            return result;
+          case typeDataItem.toLowerCase() == 'object':
+            result = getType.check(theVariable) == getType.types.object;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            return result;
+          case typeDataItem.toLowerCase() == 'array':
+            result = getType.check(theVariable) == getType.types.array;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            return result;
+          case typeDataItem.toLowerCase() == 'string':
+            result = getType.check(theVariable) == getType.types.string;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            return result;
+          case typeDataItem.toLowerCase() == 'boolean':
+            result = getType.check(theVariable) == getType.types.boolean;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            return result;
+          case typeDataItem.toLowerCase() == 'number':
+            result = getType.check(theVariable) == getType.types.number;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            return result;
+          case typeDataItem.toLowerCase() == 'date':
+            result = getType.check(theVariable) == getType.types.date;
+            if(result == false){
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            return result;
+          case getType.types[typeDataItem.toLowerCase()] != null:
+            return getType.check(theVariable) == getType.types[typeDataItem.toLowerCase()];
           default:
-            arrayRecordTypeOf.push(typeDataItem.name);
-            return false;
+            if(getType.types[typeDataItem.toLowerCase()] == undefined){
+              getType.types[typeDataItem.toLowerCase()] = getType.check(theVariable);
+              if(debug == true){
+                console.log(typeDataItem,getType.types[typeDataItem.toLowerCase()]);
+                debugger;
+              }
+              result = getType.check(theVariable) == getType.types[typeDataItem.toLowerCase()];
+              if(result == false){
+                arrayRecordTypeOf.push(typeDataItem);
+              }
+              return result;
+            }else{
+              arrayRecordTypeOf.push(typeDataItem);
+            }
+            break;
+
         }
       };
     };
     var recordTypeOf = [];
     var doCheckStaticType = closureCondition(inVariable, (recordTypeOf = []));
+    if(debug == true){
+      debugger;
+    }
     for (var a = 0; a < typeDatas.length; a++) {
-      if (doCheckStaticType(typeDatas[a]) == true) {
+      let ttt = doCheckStaticType(typeDatas[a]);
+      if(debug == true){
+        debugger;
+      }
+      if ( ttt == true) {
         isWRong = false;
         break;
       }
@@ -34,6 +133,9 @@
       var messageError = `StaticType Validation - value "${inVariable}" is Wrong type of variable, the requirement is ${JSON.stringify(recordTypeOf)}`;
       console.error("staticType - error ", messageError);
       throw new Error(messageError);
+    }
+    if(debug == true){
+      debugger;
     }
   };
   if (typeof define === 'function' && define.amd) {
